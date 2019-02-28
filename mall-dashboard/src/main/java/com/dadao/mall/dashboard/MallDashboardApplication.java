2a2579430c87d6aaa70806331652de3cf48698c0
guying4875 <zhjx2015@aliyun.com>
2019-02-28 17:02:49 +0800

    hystrix-dashboard

---
+++ mall-dashboard/src/main/java/com/dadao/mall/dashboard/MallDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.dadao.mall.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ *
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class MallDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallDashboardApplication.class, args);
+    }
+
+}
